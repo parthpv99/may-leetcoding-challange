1171c05cf02f6abc42f6e68a5c71487db48f656b
Parth Pansuriya <47769594+parthpv99@users.noreply.github.com>
2020-05-05 14:24:30 +0530

    Day 01

--- Day01 First Bad Version.cpp
+++ Day01 First Bad Version.cpp
@@ -19,24 +19,24 @@ You are given an API bool isBadVersion(version) which will return whether versio
 
 /*Solution: This Problem is basically binary search problem.*/
 
-public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        int first=1;
-        int last=n;
-        int mid=0;
-        
-        while(first<=last) {
-            mid = first + (last-first)/2;
-            
-            if(!isBadVersion(mid)) {
-                first = mid+1;
-            } else {
-                last = mid-1;
-            }
-        }
-        if(!isBadVersion(mid)) {
-            return mid+1;
-        }
-        return mid;
-    }
-}
+class Solution
+{
+public:
+	int firstBadVersion(int n)
+	{
+		int first = 1;
+		int last = n;
+		int mid = 0;
+
+		while (first <= last)
+		{
+			mid = first + (last - first) / 2;
+
+			if (!isBadVersion(mid)) {
+				first = mid + 1;
+			} else {
+				last = mid - 1;
+			}
+		}
+	}
+};
